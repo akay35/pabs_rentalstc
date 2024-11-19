2ac3bda41cec46135994ea756cb664909b011ee9
Akay <akay989@gmail.com>
2024-11-19 16:01:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ text_col, image_col = st.columns(2)   #sayfayı ikiye böldük ;)
 soundcloud_iframe = """<iframe width="100%" height="200" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/226534207&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/edgar-poe-665232219" title="edgar poe, wsea" target="_blank" style="color: #cccccc; text-decoration: none;">edgar poe, wsea</a> · <a href="https://soundcloud.com/edgar-poe-665232219/its-a-beautiful-day-white-bird" title="It&#x27;s A Beautiful Day - White Bird" target="_blank" style="color: #cccccc; text-decoration: none;">It&#x27;s A Beautiful Day - White Bird</a></div>"""
 
 # Sayfa başlığı
+with image_col:
 image_col.title("🎵 Every Ride Tells a Story!")
 
 # HTML ve CSS için bir stil bloğu  #bottom: 10px; right: 10px; 
