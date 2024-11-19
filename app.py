cbd9f0578ec691c694be56e58de7bbbb609f5c50
Akay <akay989@gmail.com>
2024-11-19 13:11:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -29,26 +29,17 @@ final_model = joblib.load('bike_rentals_model.pkl')
 
 
 # SoundCloud müzik linki
+# SoundCloud müzik linki
 soundcloud_iframe = """
-<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" 
-src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/226534207&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/226534207&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/edgar-poe-665232219" title="edgar poe, wsea" target="_blank" style="color: #cccccc; text-decoration: none;">edgar poe, wsea</a> · <a href="https://soundcloud.com/edgar-poe-665232219/its-a-beautiful-day-white-bird" title="It&#x27;s A Beautiful Day - White Bird" target="_blank" style="color: #cccccc; text-decoration: none;">It&#x27;s A Beautiful Day - White Bird</a></div>
 """
 
-# CSS ile pozisyonlandırma
-custom_css = """
-<style>
-.soundcloud-widget {
-    position: fixed;
-    bottom: 20px;
-    right: 20px;
-    width: 300px;
-    height: 150px;
-    z-index: 1000;
-}
-</style>
-<div class="soundcloud-widget">
-    {iframe}
-</div>
+# Sayfa başlığı ve açıklama
+st.title("🎵 Sürüş için Rahatlatıcı Müzik")
+st.markdown("Bisiklet sürüşünüze eşlik edecek huzurlu bir gitar melodisi.")
+
+# CSS ve HTML ile sağ alt köşeye yerleştirme
+st.markdown(soundcloud_iframe, unsafe_allow_html=True)
 """.format(iframe=soundcloud_iframe)
 
 # Sayfa başlığı ve açıklama
